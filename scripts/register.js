e3ef1735d7bcd2a8b6d5b7ecc6255deb2fcff048
neiltejada <Neiltejada86@gmail.com>
2023-10-12 21:14:34 -0700

    adding to register

--- scripts/register.js
+++ scripts/register.js
@@ -1,29 +1,35 @@
-let pets = [];
+let salon = { pets: [] };
+
+// let pets = [];
+
+//object constructor
+function Pet(name, age, gender, breed, service) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.breed = breed;
+  this.service = service;
+}
 
 function addPet() {
   const name = document.getElementById("name").value;
-  const age = document.getElementById("age").value;
-  const gender = document.getElementById("gender").value;
-  const breed = document.getElementById("breed").value;
+  const age = document.getElementById("txtAge").value;
+  const gender = document.getElementById("txtGender").value;
+  const breed = document.getElementById("txtBreed").value;
   const service = document.getElementById("service").value;
 
-  const newPet = {
-    name: name,
-    age: age,
-    gender: gender,
-    breed: breed,
-    service: service,
-  };
-  pets.push(newPet);
+  const newPet = new Pet(name, age, gender, breed, service);
+  console.log(newPet);
+  salon.pets.push(newPet);
   displayPets();
+  console.log(pets.salon);
 }
-function Pet() {}
 
 function displayPets() {
   const petList = document.getElementById("pet-list");
   petList.innerHTML = "";
 
-  pets.forEach((pet, index) => {
+  salon.pets.forEach((pet, index) => {
     const petInfo = document.createElement("div");
     petInfo.classList.add("pet-info");
     petInfo.innerHTML = `
@@ -36,6 +42,11 @@ function displayPets() {
     petList.appendChild(petInfo);
   });
 }
-function regitster() {}
 
-function init() {}
+function init() {
+  let pet1 = new Pet("Scooby", 70, "Male", "Dane");
+  let pet2 = new Pet("Scrappy,", 2, "Female", "Bully");
+  console.log(pet1);
+  console.log(pet2);
+}
+init();
